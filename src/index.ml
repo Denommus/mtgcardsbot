5c0e6ffacc000f74e8e0d35562011b4731631a69
Yuri Albuquerque <yuridenommus@gmail.com>
2017-10-29 20:40:26 -0200

    Tricking Heroku

--- src/index.ml
+++ src/index.ml
@@ -2,6 +2,9 @@ open TelegramApi
 
 let token = Sys.getenv "TOKEN" (* This value is required, it's best if the system throws when it's not found *)
 
+let port = try Sys.getenv "PORT" |> int_of_string with
+  | Not_found -> 8080
+
 let telegramBot = bot token @@ TelegramApi.options ~polling:true
 
 let () =
@@ -14,4 +17,8 @@ let () =
              |> List.filter (fun (card : Mtg.card) -> card.imageUrl <> "")
              |> List.map (fun (card : Mtg.card) -> makeInlineQueryResultPhoto ~id:card.id ~photo_url:card.imageUrl ~thumb_url:card.imageUrl ())
              |> Array.of_list))
-      |> ignore)
+      |> ignore);
+  let open Express in
+  let app = express () in
+  App.get app ~path:"/" @@ Middleware.from (fun _ res _ -> Response.sendString res "Hello, world!");
+  App.listen app ~port:port ()
