092e581e564022e1aed3d5613b8eb5b047e69e13
Yuri Albuquerque <yuridenommus@gmail.com>
2018-09-24 23:30:39 -0300

    Adding a test case.

--- __tests__/fetch_test.ml
+++ __tests__/fetch_test.ml
@@ -17,4 +17,13 @@ let () =
               let open Expect in
               let card = Array.get cards 0 in
               Js.Promise.resolve
-                (expect card.image_uris.large |> toBe "https://img.scryfall.com/cards/large/en/m15/28.jpg?1517813031"))))
+                (expect card.image_uris.large |> toBe "https://img.scryfall.com/cards/large/en/m15/28.jpg?1517813031"))));
+  describe "Tibalt image" (fun () ->
+      testPromise "Tibalt" (fun () ->
+          let open Mtg in
+          searchCard "Tibalt"
+          |> Js.Promise.then_ (fun cards ->
+              let open Expect in
+              let card = Array.get cards 0 in
+              Js.Promise.resolve
+                (expect (card.image_uris.large, card.image_uris.small) |> toEqual ("https://img.scryfall.com/cards/large/front/f/a/fa56d53c-836c-483d-988d-a288d0ad91bb.jpg?1537149837", "https://img.scryfall.com/cards/small/front/f/a/fa56d53c-836c-483d-988d-a288d0ad91bb.jpg?1537149837")))))
